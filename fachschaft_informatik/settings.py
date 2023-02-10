9f21087ae97d7f24b32052801caa343c308734ed
KilakOriginal <kilakOri@protonmail.com>
2023-02-10 20:56:32 +0100

    Updated settings

--- fachschaft_informatik/settings.py
+++ fachschaft_informatik/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = ['/etc/secrets/']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
